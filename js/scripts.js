973e49a58198db54204f496391bd58391cb60fe5
drewheathens <mudavadie@gmail.com>
2018-12-14 13:45:11 +0300

    all directories created

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// $(document).ready(function(){
+//   $("button").onclick(rollDice(event){
+function(rollDice());
+    var d1 = math.floor(math.random() *6) +1;
+
+
+    var d1 = document.getElementById(die);
+    var status = document.getElementById('status');
+
+    var dieTotal.innerHTML = d1 + d1;
+    innerHTML = "rolled" + dieTotal + ".";
+
+    if (d1 == 1) {
+      status.innerHTML += hold..
+    }
+    else if(d1 == d1) {
+      status.innerHTML += "free turn"
+    }
+    d1.innerHTML = d1;
+  });
+});
