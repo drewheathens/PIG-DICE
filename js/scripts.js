3d4fc45ebfeaaf04ab8541440610b1c2c70f8f5b
drewheathens <mudavadie@gmail.com>
2018-12-17 11:25:08 +0300

    added  objects in script

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,15 @@
-// $(document).ready(function() {
-//   $("h1#pig-dice")
-// function rollDice() {
-//
-//   var d1 = Math.floor(Math.random() * 6) +1;
-  // // var status = #status;
-  // var dieTotal = d1 + d1;
-  // alert('you rolled' + d1)
 
-   // $("#status")= "rolled" + dieTotal + ".";
- // }
-
-   // if (d1 == 1) {
-   //  #status += hold;
-   //  alert("player2 to play")
-   //  }
-   //   else if(d1 == d1) {
-   //  #status += "free turn"
-   //  }
-  // });
 function rollDice() {
+  var player1 = {
+    firstName:"";
+    secondName:"";
+    score:(die1 + die2);
+  }
+  var player2 = {
+    firstName:"";
+    secondName:"";
+    score:(die1 + die2);    
+  }
   var die1=document.getElementById('die1');
   var die2=document.getElementById('die2');
   var status = document.getElementById('status');
@@ -31,5 +22,5 @@ function rollDice() {
   if(die1==die2) {
     status.innerHTML += "free turn buddy!"
   }
-  event.preventDefault();
+
 }
