9f99e3222a2b34595ec56b37897c6ce698bb2b13
drewheathens <mudavadie@gmail.com>
2018-12-17 10:04:12 +0300

    css implemented

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,35 @@
-// $(document).ready(function(){
-//   $("button").onclick(rollDice(event){
-function(rollDice());
-    var d1 = math.floor(math.random() *6) +1;
+// $(document).ready(function() {
+//   $("h1#pig-dice")
+// function rollDice() {
+//
+//   var d1 = Math.floor(Math.random() * 6) +1;
+  // // var status = #status;
+  // var dieTotal = d1 + d1;
+  // alert('you rolled' + d1)
 
+   // $("#status")= "rolled" + dieTotal + ".";
+ // }
 
-    var d1 = document.getElementById(die);
-    var status = document.getElementById('status');
-
-    var dieTotal.innerHTML = d1 + d1;
-    innerHTML = "rolled" + dieTotal + ".";
-
-    if (d1 == 1) {
-      status.innerHTML += hold..
-    }
-    else if(d1 == d1) {
-      status.innerHTML += "free turn"
-    }
-    d1.innerHTML = d1;
-  });
-});
+   // if (d1 == 1) {
+   //  #status += hold;
+   //  alert("player2 to play")
+   //  }
+   //   else if(d1 == d1) {
+   //  #status += "free turn"
+   //  }
+  // });
+function rollDice() {
+  var die1=document.getElementById('die1');
+  var die2=document.getElementById('die2');
+  var status = document.getElementById('status');
+  var Die1= Math.floor(Math.random() * 6) +1;
+  var Die2= Math.floor(Math.random() * 6) +1;
+  var total= die+die;
+  die1.innerHTML = Die1;
+  die2.innerHTML = Die2;
+  status.innerHTML "played" + total;
+  if(die1==die2) {
+    status.innerHTML += "free turn buddy!"
+  }
+  event.preventDefault();
+}
